a33d01e366f2cd7655ff7f0ea6546c42e3008bed
Florian Kahllund <flo.kahllund@gmail.com>
2016-05-29 09:26:41 +0100

    updating the testing main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,25 +1,18 @@
 extern crate sqlib;
 
-use sqlib::{Connection, ChannelList, ClientList};
-use sqlib::map::*;
+use sqlib::Connection;
 
 fn main() {
     let mut conn = Connection::new("127.0.0.1:10011".to_string()).unwrap();
     conn.send_command("use 1").unwrap();
     conn.send_command("login serveradmin hqjI+wn7").unwrap();
-    let mut clients = conn.send_command("clientlist").unwrap().parse::<ClientList>().unwrap();
 
-    for client in clients.get_mut().iter_mut() {
-        let command = format!("clientinfo clid={}", client.clid);
-        let str = conn.send_command(command).unwrap();
-        let map = to_map(&str);
-        client.mut_from_map(&map);
-    }
+    let channels = conn.channellist_with_clients().unwrap();
 
-    let mut channels = conn.send_command("channellist").unwrap().parse::<ChannelList>().unwrap();
-    channels.merge_clients(&clients);
     for channel in channels.iter() {
-        println!("{}", channel);
+        if !channel.is_empty() {
+            println!("{}", channel);
+        }
     }
     let _ = conn.quit();
 }
