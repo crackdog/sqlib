30fd91925db3f6deabea1ed78642cce9aeded703
Florian Kahllund <flo.kahllund@gmail.com>
2016-05-30 19:45:44 +0200

    adding a hyper test to bin/main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,18 +1,40 @@
 extern crate sqlib;
+extern crate hyper;
 
-use sqlib::Connection;
+use hyper::server::{Server, Request, Response};
+use std::io::Write;
+use hyper::header::ContentLength;
+
+fn handle(req: Request, mut res: Response) {
+    let body = "Hello World!";
+    res.headers_mut().set(ContentLength(body.len() as u64));
+    let mut res = res.start().unwrap();
+    // res.write_all(body).unwrap();
+    write!(req, "{:?}", body);
+
+    println!("{:?}", req.remote_addr);
+    println!("{:?}", req.uri);
+}
 
 fn main() {
-    let mut conn = Connection::new("127.0.0.1:10011".to_string()).unwrap();
-    conn.send_command("use 1").unwrap();
-    conn.send_command("login serveradmin hqjI+wn7").unwrap();
-
-    let channels = conn.channellist_with_clients().unwrap();
-
-    for channel in channels.iter() {
-        if !channel.is_empty() {
-            println!("{}", channel);
-        }
-    }
-    let _ = conn.quit();
+    Server::http("0.0.0.0:8080").unwrap().handle(handle).unwrap();
 }
+
+// use sqlib::Connection;
+//
+// fn main() {
+//    let mut conn = Connection::new("127.0.0.1:10011".to_string()).unwrap();
+//    conn.send_command("use 1").unwrap();
+//    conn.send_command("login serveradmin hqjI+wn7").unwrap();
+//
+//    let channels = conn.channellist_with_clients().unwrap();
+//
+//    for channel in channels.iter() {
+//        if !channel.is_empty() {
+//            println!("{}", channel);
+//        }
+//    }
+//
+//    println!("{}", channels.as_json());
+//    let _ = conn.quit();
+// }
