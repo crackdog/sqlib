eb3c85eea36a8f17cefecb3048c38829eccda28a
Florian Kahllund <flo.kahllund@gmail.com>
2016-06-09 13:25:30 +0200

    adding mod prelude

---
+++ src/prelude.rs
@@ -0,0 +1,8 @@
+// mod prelude
+
+pub use client::{Client, ClientList};
+pub use channel::{Channel, ChannelList};
+pub use connection::Connection;
+pub use command::Command;
+
+pub use error::{Result, Error, SQError};
