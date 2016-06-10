42d8c2d4599c90221bd2442e5479df042b764b9b
Florian Kahllund <flo.kahllund@gmail.com>
2016-06-10 23:55:27 +0300

    exporting types and functions from map

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,8 +1,9 @@
-// mod prelude
+//! The prelude exports all important structs and functions of sqlib.
 
 pub use client::{Client, ClientList};
 pub use channel::{Channel, ChannelList};
 pub use connection::Connection;
 pub use command::Command;
+pub use map::{StringMap, to_map, update_from_map};
 
 pub use error::{Result, Error, SQError};
